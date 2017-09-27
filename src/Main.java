d0f3f6e56beb984f8ad21467409ed45e253013e9
s22194q23687 <s22194q23687@gmail.com>
2017-09-27 20:13:26 +0800

    888

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("106024580蘇佩怡");
+	System.out.print("106024580");
+	System.out.print("Su Pei Yi");
+	System.out.print("Asia University");
         // write your code here
     }
 }
